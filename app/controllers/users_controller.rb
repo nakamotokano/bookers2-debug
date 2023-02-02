f453cf382044e6484041756cbc006df6b37a82e0
nakamotokano <121929712+nakamotokano@users.noreply.github.com>
2023-02-02 05:50:52 +0000

    202302

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:update]
+  before_action :set_user, only: [:followings, :followers]
+
 
   def show
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books
   end
+
 
   def index
     @users = User.all
@@ -21,7 +24,7 @@ class UsersController < ApplicationController
   end
 
   def update
-@user = current_user
+       @user = current_user
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
@@ -37,8 +40,19 @@ class UsersController < ApplicationController
     end
    end
 
-  private
+  def followings
+    @users = @user.followings
+  end
 
+  def followers
+    @users = @user.followers
+  end
+
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
